33731f27a4429345e92736d74476d7b5ee7c7d5c
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2024-04-10 18:58:45 -0600

    Agregar archivos de proyecto.

---
+++ Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidades
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; } 
+    }
+}
